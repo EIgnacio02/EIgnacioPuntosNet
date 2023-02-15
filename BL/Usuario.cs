37e3ca70e48baa4f8e7560b41e66b6a760840465
Efrain Ignacio <efrainignaciohernandez@gmail.com>
2023-02-15 16:34:07 -0600

    Add project files.

---
+++ BL/Usuario.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.EIgnacioPuntosNetEntities context = new DL.EIgnacioPuntosNetEntities())
+                {
+                    var query = context.UsuarioGetAll().ToList();
+                    result.Objects = new List<object>();
+
+                    if (query!=null)
+                    {
+                        foreach (var obj in query)
+                        {
+                            ML.Usuario usuario= new ML.Usuario();
+
+                            usuario.IdUsuario = obj.IdUsuario;
+                            usuario.Nombre = obj.Nombre;
+                            usuario.ApellidoPaterno= obj.ApellidoPaterno;
+                            usuario.ApellidoMaterno= obj.ApellidoMaterno;
+                            usuario.FechaNacimiento= obj.FechaNacimiento;
+                            usuario.Email=obj.Email;
+                            usuario.Password=obj.Password;
+                            result.Objects.Add(usuario);
+                        }
+                    }
+                    result.Correct = true; 
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+
+        public static ML.Result GetById(int IdUsuario)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.EIgnacioPuntosNetEntities context = new DL.EIgnacioPuntosNetEntities())
+                {
+                    var query  = context.UsuarioGetById(IdUsuario).SingleOrDefault();
+                    result.Objects = new List<object>();
+
+                    if (query!=null)
+                    {
+                        ML.Usuario usuario = new ML.Usuario();
+
+                        usuario.IdUsuario = query.IdUsuario;
+                        usuario.Nombre = query.Nombre;
+                        usuario.ApellidoPaterno = query.ApellidoPaterno;
+                        usuario.ApellidoMaterno = query.ApellidoMaterno;
+                        usuario.FechaNacimiento = query.FechaNacimiento;
+                        usuario.Email = query.Email;
+                        usuario.Password = query.Password;
+
+                        result.Object= usuario;
+                    }
+
+                }
+
+                result.Correct = true;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return result;
+        }
+
+        public static ML.Result Add(ML.Usuario usuario)
+        {
+            ML.Result result= new ML.Result();
+
+            try
+            {
+                using (DL.EIgnacioPuntosNetEntities context= new DL.EIgnacioPuntosNetEntities())
+                {
+                    var query = context.UsuarioAdd(usuario.Nombre,usuario.ApellidoPaterno,usuario.ApellidoMaterno,usuario.FechaNacimiento,usuario.Email,usuario.Password);
+                    result.Objects=new List<object> ();
+                    if (query>0)
+                    {
+                        result.Message = "Se ingresaron los datos correctament";
+                    }
+                }
+                result.Correct= true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return result;
+        }
+
+        public static ML.Result Login(string Email)
+        {
+            ML.Result result = new ML.Result();
+            ML.Usuario usuario = new ML.Usuario();
+            try
+            {
+                using (DL.EIgnacioPuntosNetEntities context = new DL.EIgnacioPuntosNetEntities())
+                {
+
+
+                    var query=context.LoginUser(Email).SingleOrDefault();
+                    result.Objects = new List<object>();
+                    if (query != null)
+                    {
+
+                        usuario.Email = query.Email;
+                        usuario.Password = query.Password;
+
+                        result.Object = usuario;
+                    }
+                }
+                result.Correct = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult GetAll()
+        {
+            ML.Result result = BL.Usuario.GetAll();
+            ML.Usuario usuario= new ML.Usuario();
+            if (result.Correct)
+            {
+                usuario.UsuarioList = result.Objects;
+                return View(usuario);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Form(int? IdUsuario)
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            if (IdUsuario==null)
+            {
+                return View(usuario);
+            }
+            else
+            {
+
+                ML.Result result = BL.Usuario.GetById(IdUsuario.Value);
+                if (result.Correct)
+                {
+                    usuario = (ML.Usuario)result.Object;
+                }
+                else
+                {
+                    ViewBag.Message = "Ocurrio un error al consultar los datos del usuario";
+                }
+                return View(usuario);
+
+            }
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Usuario usuario)
+        {
+            if (usuario.IdUsuario == 0)
+            {
+                ML.Result result = BL.Usuario.Add(usuario);
+                if (result.Correct)
+                {
+                    ViewBag.Message = result.Message;
+                }
+                else
+                {
+                    ViewBag.Mensaje = "No ha registrado el usuario" + result.Message;
+                }
+            }
+            return PartialView("Modal");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Email, string Password)
+        {
+            ML.Result result = BL.Usuario.Login(Email);
+            ML.Usuario usuario= new ML.Usuario();
+
+            usuario=(ML.Usuario)result.Object;
+            if (result.Correct)
+            {
+
+                if (usuario.Email == Email && usuario.Password == Password)
+                {
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    ViewBag.Message = "email o password incorrectos ";
+                    return PartialView("ModalLogin");
+                }
+            }
+            else
+            {
+                ViewBag.Message = "email o password incorrectos ";
+                return PartialView("ModalLogin");
+            }
+        }
+    }
+}
